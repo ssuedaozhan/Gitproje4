b4e1a41c83e50640f71e7daac50d984c0d7394e7
ssuedaozhan <suedaozhan1998@gmail.com>
2023-01-27 15:37:18 +0200

    task 2  yapildi

--- src/Gun01/Student.java
+++ src/Gun01/Student.java
@@ -3,5 +3,8 @@ package Gun01;
 public class Student {
     public static void main(String[] args) {
         System.out.println("merhaba1 ");
+
+        System.out.println("task2 yapildi");
+
     }
 }
